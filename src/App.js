d2bfb07c23586117dff3fc72d6d7dd84ba9f53fd
Nischal Mainali <jemsmainali@gmail.com>
2025-01-21 08:48:28 +0545

    fix: navigation using hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
