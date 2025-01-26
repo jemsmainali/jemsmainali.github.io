4e03600210f9487a79d0e9e4ee7420bbd92ecdba
Nischal Mainali <jemsmainali@gmail.com>
2025-01-26 08:48:24 +0545

    fixed hashrouter

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,17 +13,17 @@ const Nav = () => {
           <a href="/">Home</a>
         </li>
         <li>
-          <a href="/about">About</a>
+          <a href="/#/about">About</a>
         </li>
         <li>
-          <a href="/services">Services</a>
+          <a href="/#/services">Services</a>
         </li>
         <li>
-          <a href="/projects">Projects</a>
+          <a href="/#/projects">Projects</a>
         </li>
         
         <li>
-          <a href="/contact">Contact</a>
+          <a href="/#/contact">Contact</a>
         </li>
       </ul>
     </nav>
